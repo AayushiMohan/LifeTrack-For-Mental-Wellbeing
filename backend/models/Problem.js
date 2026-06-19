075de7665fe30bc703976e4c95f76dc727f60ef9
ByteSpace <aayushimohan118@gmail.com>
2026-06-19 23:29:38 +0530

    Create Problem.js

---
+++ backend/models/Problem.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const problemSchema = new mongoose.Schema({
+  userEmail: String,
+  anonymousName: String,
+  category: String,
+  title: String,
+  description: String,
+  connectRequests: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Problem", problemSchema);
